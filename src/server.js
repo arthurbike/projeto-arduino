cfc28bc0e3f8861a508f49218eb846a1b48113b6
arthurbike <arthur.7martins@gmail.com>
2020-12-02 22:03:42 -0300

    projeto finalizado

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 
+const cors = require("cors");
 const path = require("path");
 const express = require("express");
-const cors = require("cors");
 const five = require("johnny-five");
 const board = new five.Board({ port: "/dev/ttyUSB0", repl: false });
 
 const SERVER_PORT = process.env.SERVER_PORT;
-const MAX_CLIENTS_COUNT = 2;
 
 let clients = [];
 let clientId = "";
@@ -19,59 +18,96 @@ app.use(cors());
 app.set("trust proxy", true);
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 
-// arduino settings
-let ledState = false;
-
-board.on("ready", () => {
-  console.log("Board ready!");
-  const led = new five.Led(13);
-
-  const toogleLed = () => {
-    ledState ? led.on() : led.off();
-  };
-
-  board.loop(100, toogleLed);
-});
-
 // routes
 app.get("/", (req, res) => res.sendFile("index.html"));
 
 // only accepts requests on the default/home endpoint
 app.all("*", (req, res) => res.status(404).send({ message: "Not Found" }));
 
-// socket functionalities
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+// arduino settings
+let ledState = false;
+let led;
 
-const removeClient = (client) =>
-  (clients = clients.filter((x) => clients[client]));
+const changeState = () => {
+  ledState = !ledState;
+  if (ledState) {
+    led.on();
+  } else {
+    led.off();
+  }
+};
 
-io.on("connection", (socket) => {
-  clientId = socket.id;
-  clients.push(clientId);
-  io.sockets.emit("newClient", clientId);
+board.on("ready", () => {
+  console.log("Board ready!");
 
-  socket.on("disconnect", () => {
-    removeClient(clientId);
-    console.log(`CLIENT DISCONNECTED: ${clientId}
+  led = new five.Led.RGB({
+    pins: {
+      red: 11,
+      green: 10,
+      blue: 9,
+    },
+    isAnode: true,
+  });
+
+  button = new five.Button(2);
+
+  button.on("down", () => {
+    changeState();
+  });
+
+  button.on("up", () => {
+    changeState;
+  });
+
+  led.on();
+  led.color("#FF0000");
+
+  // socket functionalities
+  const server = require("http").createServer(app);
+  const io = require("socket.io")(server);
+
+  const removeClient = (client) =>
+    (clients = clients.filter((x) => clients[client]));
+
+  io.on("connection", (socket) => {
+    clientId = socket.id;
+    clients.push(clientId);
+    io.sockets.emit("newClient", clientId);
+
+    socket.on("disconnect", () => {
+      removeClient(clientId);
+      console.log(`CLIENT DISCONNECTED: ${clientId}
     List of clients: [${clients}];`);
-    io.sockets.emit("message", `CLIENT DISCONNECTED: ${clientId}`);
-  });
+      io.sockets.emit("message", `CLIENT DISCONNECTED: ${clientId}`);
+    });
 
-  socket.on("toogleLed", () => {
-    ledState = !ledState;
-    console.log(ledState);
-  });
+    socket.on("toogleLed", () => {
+      ledState = !ledState;
+      if (ledState) {
+        led.on();
+      } else {
+        led.off();
+      }
+      console.log(ledState);
+    });
 
-  console.log(clients);
+    socket.on("color_changed", (color) => {
+      led.color(color);
+      console.log(color);
 
-  io.sockets.emit(
-    "message",
-    `New client, id: ${clientId};
+      io.sockets.emit("color_changed", color);
+    });
+
+    console.log(clients);
+
+    io.sockets.emit(
+      "message",
+      `New client, id: ${clientId};
     List of clients: [${clients}];`
+    );
+  });
+
+  server.listen(SERVER_PORT, () =>
+    console.log(`Server listening on port ${SERVER_PORT}`)
   );
 });
-
-server.listen(SERVER_PORT, () =>
-  console.log(`Server listening on port ${SERVER_PORT}`)
-);
